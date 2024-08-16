aae775c1cc432ed3ab44c18bc69d929e9af0f544
fred-hu <hufei_yx@sina.com>
2024-08-16 19:03:34 +0800

    feat: style

--- src/app.vue
+++ src/app.vue
@@ -4,8 +4,8 @@
       <ElSelectAll v-model="storeCodes" filterable multiple collapse-tags @change="onChange" :options="mdoptionsList" :props="{ label: 'deviceName', value: 'deviceNo' }" :selectAll="true" :withAll="true" :size="'mini'" style="width: 300px" />
     {{ storeCodes }}
     </div>
-    <div>
-      <ElSelectAll v-model="storeCodes2" filterable multiple collapse-tags @change="onChange2" :options="mdoptionsList2" :selectAll="false" :withAll="true" :size="'mini'" style="width: 300px" />
+    <div style="margin-top: 20px;">
+      <ElSelectAll v-model="storeCodes2" filterable multiple collapse-tags @change="onChange2" :options="mdoptionsList2" :selectAll="false" :withAll="false"  style="width: 300px" />
     {{ storeCodes2 }}
     </div>
   </div>
