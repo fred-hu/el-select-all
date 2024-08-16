7c1a90a9df1fd2897440a854c5f0897db78e7322
fredfhu <fredfhu@tencent.com>
2024-08-16 16:18:10 +0800

    feat: v0.4.0

--- src/app.vue
+++ src/app.vue
@@ -1,27 +1,39 @@
 <template>
   <div id="el-select-all">
     <div>
-      <ElSelectAll v-model="storeCodes" filterable multiple collapse-tags @change="onChange" :options="mdoptionsList" />
-      {{ storeCodes }}
+      <ElSelectAll v-model="storeCodes" filterable multiple collapse-tags @change="onChange" :options="mdoptionsList" :props="{ label: 'deviceName', value: 'deviceNo' }" :selectAll="true" :withAll="true" :size="'mini'" style="width: 300px" />
+    {{ storeCodes }}
     </div>
     <div>
-      <ElSelectAll v-model="storeCodes2" filterable multiple collapse-tags :widthAll="false" @change="onChange2" :options="mdoptionsList" />
-      {{ storeCodes2 }}
+      <ElSelectAll v-model="storeCodes2" filterable multiple collapse-tags @change="onChange2" :options="mdoptionsList2" :selectAll="false" :withAll="true" :size="'mini'" style="width: 300px" />
+    {{ storeCodes2 }}
     </div>
-    
   </div>
 </template>
-
 <script>
+import ElSelectAll from '@/components/ElSelectAll.vue'
 export default {
   name: 'App',
-  components: {
-  },
+  components: { ElSelectAll },
   data() {
     return {
-      storeCodes: ['test'],
+      storeCodes: ['testdeviceNo'],
       storeCodes2: ['test'],
-      mdoptionsList: [{
+      mdoptionsList: [
+        {
+          deviceNo: 'testdeviceNo',
+          deviceName: 'testdeviceName',
+        },
+        {
+          deviceNo: 'test2deviceNo',
+          deviceName: 'test2deviceName',
+        },
+        {
+          deviceNo: 'test3deviceNo',
+          deviceName: 'test3deviceName',
+        },
+      ],
+      mdoptionsList2: [{
         key: 'test',
         value: 'test',
         label: 'test'
@@ -43,9 +55,8 @@ export default {
     },
     onChange2(val) {
       console.log(val)
-      console.log(this.storeCodes2)
-    }
-  }
+    },
+  },
 }
 </script>
 
